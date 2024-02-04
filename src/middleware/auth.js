45321081d83650c7baf9a7fc356371578e4dd396
himank singh <singhhimank241@gmail.com>
2024-02-04 22:41:30 +0530

    added try catch in middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,9 +1,15 @@
 module.exports = function(req, res, next){
-    const requestToken = req.header('Authorization').split('Bearer ')[1];
-    const expectedToken = process.env.ACCESS_TOKEN ;
-    if (requestToken === expectedToken) {
-        next();
-    } else {
-        res.status(401).json({ error: 'Unauthorized: Access token mismatch' });
+    try{
+
+        const requestToken = req.header('Authorization').split('Bearer ')[1];
+        const expectedToken = process.env.ACCESS_TOKEN ;
+        if (requestToken === expectedToken) {
+            next();
+        } else {
+            res.status(401).json({ error: 'Unauthorized: Access token mismatch' });
+        }
+    }
+    catch(err){
+        res.status(500).send(err?.message)
     }
 }
